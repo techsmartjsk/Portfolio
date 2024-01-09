be80d5750cee7960fda5bf7c6b0cd3670e9dcad8
Jaskirat Singh <techsmartjsk@gmail.com>
2024-01-09 12:52:10 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import Home from './pages/home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
